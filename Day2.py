fc420f678956c37f5c4aac00d6cc6bf1a6022760
tamanna-c <77087383+tamanna-c@users.noreply.github.com>
2021-06-04 00:07:43 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,80 @@
+#Task-1
+"""print("Hello World")"""
+''' This is an example of multiline comment'''
+""" This is also an example of multiline comment"""
+
+#Task-2
+"""
+a=10 
+b=20.5
+c="Tamanna"
+
+print(a)
+print("Value of b is:",b)
+print("My name is",c)"""
+
+#Task-3
+"""name="Tamanna"
+print("Name is:",name)
+#assigning a new value 
+name="Tamanna.com"
+print("Name is:",name) """
+
+#Task-4
+"""a,b,c=20,10.5,"Orange"
+print(a)
+print("Value of b is:",b)
+print("Fruit name is:",c) """
+
+#Task-5
+"""b=c=d=900
+print("Value of b=",b)
+print("Value of c=",c)
+print("Value of d=",d) """
+
+#Task-6
+"""n1=100
+print(n1,"is of type", type(n1))
+
+n2=20.6
+print(n2,"is of type", type(n2))
+print(n2, "is complex number?", isinstance(10.5,int))
+
+n3=1+3j
+print(n3, "is complex number?", isinstance(1+3j,complex)) """
+
+#Task-7
+"""name="Tamanna Choithani"
+print("Name is :",name)
+print(name [0])
+print(name [2:5])
+print(name[2:])
+print(name[:4])
+print(name*2)
+print (name + " Hello")  """
+
+#Task-8
+"""list1 = [10, 20, 30, "Akash", 40, 50, "Technolabs", 60]
+print(list1)
+print (list1[2])
+print(list1[0:3])
+print (list1 [5:])
+print (list1[:3])
+print(type(list1)) """
+
+#Task-9
+"""d = { 1: 'Satoru',2: 'Suguru', 'key': 10 }
+print (type(d))
+
+print ("d[1] = ", d[1])
+print ("d[2] = ", d[2])
+print ("d[key] = ", d['key']) """
+
+#Task-10
+"""l1 = (10, 20, 30, "Akash", 40, 50, "Technolabs", 60)
+print(l1)
+print (l1[2])
+print(l1[0:3])
+print (l1 [5:])
+print (l1[:3])
+print(type(l1)) """
