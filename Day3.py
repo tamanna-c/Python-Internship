3f17fbd3e227acbc152065374fdcaef900e7249d
tamanna-c <77087383+tamanna-c@users.noreply.github.com>
2021-06-04 00:08:17 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,111 @@
+#Task-1
+"""a=int(input("Enter first no:"))
+b=int(input("Enter second no:"))
+ans=a+b
+print("Ans is:",ans)"""
+
+#Task-2
+"""n1=200
+n2=30
+if n1>n2:
+    print("n1 is greater")
+
+if n1<n2:
+    print("n2 is greater") """
+
+#Task-3
+"""n1=50
+n2=30
+if n1>n2:
+    print("n1 is greater")
+else:
+    print("n2 is greater")"""
+
+#Task-4
+"""n1=149
+n2=309
+if n1==n2:
+    print("Both n1 and n2 are equal")
+
+elif n1>n2:
+    print("n1 is greater")    
+
+else:
+    print("n2 is greater") """
+
+#Task-5
+"""n1=102
+
+if n1>=0:
+    if n1==0:
+        print("n1 is Zero")
+    else: 
+        print("n1 is Positive")
+else:
+    print("n1 is Negative") """
+
+#Task-6
+"""i=1
+while i<=10:
+     print("value is ", i)
+     i +=1 """
+
+#Task-7
+"""for i in "Hello":
+    print(i) """
+
+#Task-8
+"""l1=[50,70,'hey!']
+print(l1)
+for i in l1:
+    print("value is ",i) """
+
+#Task-9
+"""for i in range(10):
+    print("value is", i)
+print("These are values of i\n")
+
+for j in range(1,5):
+    print("value is", j)
+print("These are values of j\n")
+
+for k in range(1,10, 2):
+    print("value is", k)
+print("These are values of k\n") """
+
+#Task-10
+"""list1 = [10, 20, "Akash Technolabs"]
+
+for i in range(len(list1)):
+    print('Value is :', list1[i]) """
+
+#Task-11
+"""a=[12,14,"happy"]
+print(a)
+
+for i in a:
+    print("Value is ",i)
+
+for j in range(len(a)):
+    print("Value is ",a[j]) """
+
+#Task-12
+"""list1 = [10, 20, "Netflix"]
+for i in range(len(list1)):
+    print('Value is :', list1[i])
+else:
+        print("No elements left.") """
+
+#Task-13
+"""i=0
+while i<10:
+    print("Value is:",i)
+    i +=1
+    if i>=5:
+        break """
+
+#Task-14
+"""for x in range(10):
+    if x%2==0:
+        continue
+    print("Value is:",x) """
