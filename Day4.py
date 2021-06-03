2b87a3b8eeac4b15cdc812b1419a9c59808a5811
tamanna-c <77087383+tamanna-c@users.noreply.github.com>
2021-06-04 00:08:37 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,72 @@
+#Task-1
+"""def myfunction(name):
+    print("Hello World")
+    print("Name is:",name)
+
+myfunction("Tamanna")"""
+
+#Task-2
+"""def myfunction():
+ name="Shaya"
+ n1=70
+ return  name,n1
+
+name,n1=myfunction()
+print("Name is ",name)
+print("n1 is ",n1)"""
+
+#Task-3
+"""def calSum(n1=190, n2=900):
+    print(n1+n2)
+
+calSum(10,20)
+calSum() """
+
+#Task-4
+"""def sum(a,b):
+    print("Sum is:",a+b)
+sum(b=240,a=20) """
+
+#Task-5
+"""def calSum(n1, n2):
+    print(n1+n2)
+
+calSum(n2=46, n1=27) """
+
+#Task-6
+"""def sum(*n1):
+    sum=0
+    for i in n1:
+        sum=sum+i
+    print("Ans is ",sum)
+sum(11,30)
+sum(10,20,30)"""
+
+#Task-7
+"""def myfunction(**arg):
+    for i,j in arg.items():
+        print(j)
+
+myfunction(name="Tara", nm="Yuta")"""
+
+#Task-8
+"""def add(*num):
+    sum=0
+    for n in num:
+        sum=sum+n
+    print("Sum:",sum)
+add(120,30)
+add(23,12,45) """
+
+#Task-9
+"""def my_func():
+    x=110
+    print("Value inside function:",x)
+x=240
+my_func()
+print("Value outside function:",x) """
+
+#Task-10
+"""import demo 
+name=demo.myfunction("Dhara")
+print("Name is ",name) """
